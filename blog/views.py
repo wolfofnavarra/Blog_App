974a053cf3e17d9103e58a18da1309bf5297ad38
wolfofnavarra <jfmocnavarro@gmail.com>
2023-01-23 21:27:37 -0600

    Added comments to the proyects

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import CommentForm
+from django.views import generic
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status = 1).order_by('-created_on')
-    template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
+    #paginate_by = 3
 
-class PostDetail(generic.DetailView):
-    model = Post
+def post_detail(request, slug):
     template_name = 'post_detail.html'
+    post = get_object_or_404(Post, slug = slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False) #create comment object but dont save to database yet.
+            new_comment.post = post # assign current post to comment
+            new_comment.save() # save the comment to the database
+
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+#TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], #TEMPLATE_DIRS
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +72,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
